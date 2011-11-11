dfa3a688ce35317a74a9fd516d03a71b387f1837
Daniele Sdei <danielesdei@gmail.com>
2011-11-11 12:55:45 +0100

    added Daisy4 library

---
+++ Daisy4/Daisy4.cpp
@@ -0,0 +1,109 @@
+#include "Daisy4.h"
+
+#include <WProgram.h>
+
+
+Daisy4::Daisy4(int wing, int dipswitch) {
+	_wing=wing;
+	_dipswitch=dipswitch + 1;
+
+}
+
+void Daisy4::begin() {
+	if(_wing == W2) {
+		if (_dipswitch >= 5 && _dipswitch <= 8)
+			pinMode(Wing2(_dipswitch),OUTPUT);
+	}
+	else if(_wing == W3) {
+		if (_dipswitch >= 2 && _dipswitch <= 8)
+			pinMode(Wing3(_dipswitch),OUTPUT);
+	}
+	else if(_wing == W4) {
+		if (_dipswitch >= 2 && _dipswitch <= 8)
+			pinMode(Wing4(_dipswitch),OUTPUT);
+	}
+}
+
+int Daisy4::readState(void) {
+	switch (_wing) {
+		case W2:
+			return digitalRead(Wing2(_dipswitch));
+		case W3:
+			return digitalRead(Wing3(_dipswitch));
+		case W4:
+			return digitalRead(Wing4(_dipswitch));
+	}
+}
+
+void Daisy4::relayOn(void) {
+	switch (_wing) {
+		case W2:
+			digitalWrite(Wing2(_dipswitch), HIGH);
+		case W3:
+			digitalWrite(Wing3(_dipswitch), HIGH);
+		case W4:
+			digitalWrite(Wing4(_dipswitch), HIGH);
+	}
+}
+
+void Daisy4::relayOff(void) {
+	switch (_wing) {
+		case W2:
+			digitalWrite(Wing2(_dipswitch), LOW);
+		case W3:
+			digitalWrite(Wing3(_dipswitch), LOW);
+		case W4:
+			digitalWrite(Wing4(_dipswitch), LOW);
+	}
+}
+
+int Daisy4::Wing2(int pin) {
+	switch (pin) {
+		case 5:
+			return(W2_5);
+		case 6:
+			return(W2_6);
+		case 7:
+			return(W2_7);
+		case 8:
+			return(W2_8);
+	}
+}
+
+int Daisy4::Wing3(int pin) {
+	switch (pin) {
+		case 2:
+			return(W3_2);
+		case 3:
+			return(W3_3);
+		case 4:
+			return(W3_4);
+		case 5:
+			return(W3_5);
+		case 6:
+			return(W3_6);
+		case 7:
+			return(W3_7);
+		case 8:
+			return(W3_8);
+	}
+}
+
+int Daisy4::Wing4(int pin) {
+	switch (pin) {
+		case 2:
+			return(W4_2);
+		case 3:
+			return(W4_3);
+		case 4:
+			return(W4_4);
+		case 5:
+			return(W4_5);
+		case 6:
+			return(W4_6);
+		case 7:
+			return(W4_7);
+		case 8:
+			return(W4_8);
+	}
+}
---
+++ Daisy4/Daisy4.h
@@ -0,0 +1,53 @@
+#ifndef Daisy4_h
+#define Daisy4_h
+
+#include <WProgram.h>
+
+
+#define W2 2
+#define W3 3
+#define W4 4
+
+#define W2_5 14
+#define W2_6 15
+#define W2_7 16
+#define W2_8 17
+
+#define W3_2 11
+#define W3_3 12
+#define W3_4 13
+#define W3_5 10
+#define W3_6 9
+#define W3_7 5
+#define W3_8 6
+
+#define W4_2 8
+#define W4_3 7
+#define W4_4 2
+#define W4_5 3
+#define W4_6 4
+#define W4_7 18
+#define W4_8 19
+
+
+class Daisy4 {
+	public:
+		Daisy4(int wing, int dipswitch);
+		void begin();
+		int readState(void);
+		void relayOn(void);
+		void relayOff(void);
+
+	private:
+		int _wing;
+		int _dipswitch;
+		int Wing2(int pin);
+		int Wing3(int pin);
+		int Wing4(int pin);
+};
+
+#endif
+
+
+
+	
